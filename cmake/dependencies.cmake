c0941a5b3759e3fbfd74c3c0f7ae0ba25d4d1664
Simon Leiner <simon@leiner.me>
2021-12-30 00:10:13 +0100

    Build installer for macOS
    Signed-off-by: Simon Leiner <simon@leiner.me>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -34,5 +34,11 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/juce-framework/JUCE.git
   GIT_TAG ddaa09110392a4419fecbb6d3022bede89b7e841 # 6.1.4
 )
-FetchContent_MakeAvailable(juce)
+
+# JUCE registers the built SDK for installation. But as for our own installers,
+# we do not want a JUCE SDK to be included, we add the "EXCLUDE_FROM_ALL"
+# parameter to the JUCE subdirectory being added. This has the notable effect
+# that install commands are ignored.
+FetchContent_Populate(juce)
+add_subdirectory(${juce_SOURCE_DIR} ${juce_BINARY_DIR} EXCLUDE_FROM_ALL)
 list(POP_BACK CMAKE_MESSAGE_INDENT)
---
+++ cmake/installer.cmake
@@ -0,0 +1,98 @@
+# Global Parameters (must be set before CPack is initialized)
+# ##############################################################################
+
+# Installation Prefixes
+# ---------------------
+
+# The plugins are always installed into the folder $PREFIX/$PLUGIN_FORMAT. The
+# prefixes differ by operating system.
+if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+  # On macOS, we install the plugins into the _global_ folder. This makes the
+  # plugin available to all users and, according to Steinberg, is the much more
+  # common location for installing plugins.
+  #
+  # Source: https://helpcenter.steinberg.de/hc/en-us/articles/115000171310
+  # Permalink:
+  # https://web.archive.org/web/20210415101713/https://helpcenter.steinberg.de/hc/en-us/articles/115000171310
+  set(GLOBAL_PLUGIN_INSTALL_PREFIX "/Library/Audio/Plug-Ins")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+  # On 64-bit Windows (which is the only version currently supported), we can
+  # install all plugins into "C:\Program Files\Common Files". However, we do not
+  # hardcode this folder, but instead rely on the environment variable
+  # "COMMONPROGRAMFILES", which works even if the system drive is not at drive
+  # letter C.
+  #
+  # Sources:
+  # https://helpcenter.steinberg.de/hc/en-us/articles/115000177084-VST-plug-in-locations-on-Windows
+  # https://docs.microsoft.com/en-us/windows/deployment/usmt/usmt-recognized-environment-variables
+  # Permalink:
+  # https://web.archive.org/web/20211019091119/https://helpcenter.steinberg.de/hc/en-us/articles/115000177084-VST-plug-in-locations-on-Windows
+  # https://web.archive.org/web/20211031002343/https://docs.microsoft.com/en-us/windows/deployment/usmt/usmt-recognized-environment-variables
+  set(GLOBAL_PLUGIN_INSTALL_PREFIX "$ENV{CommonProgramFiles\(x86\)}")
+else() # Linux
+  # For Linux, there does not seem to exist a common standard for where to put
+  # VST plugins. So, we just pick a default path that suits us.
+  set(GLOBAL_PLUGIN_INSTALL_PREFIX "/usr/lib/pizmidi")
+endif()
+
+set(CPACK_PACKAGING_INSTALL_PREFIX
+    ${GLOBAL_PLUGIN_INSTALL_PREFIX}
+    CACHE FILEPATH "Installation prefix for installers being built")
+
+if(${CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT})
+  set(CMAKE_INSTALL_PREFIX
+      ${GLOBAL_PLUGIN_INSTALL_PREFIX}
+      CACHE FILEPATH
+            "Installation prefix when installing from a dev environment")
+endif()
+
+# UI Tweaks
+# ---------
+
+set(CPACK_PACKAGE_VENDOR
+    "Pizeo"
+    CACHE STRING "Name of the package vendor")
+set(CPACK_PACKAGE_DESCRIPTION
+    "Useful tools for audio production and MIDI manipulation"
+    CACHE STRING "A description of the project, used in graphical installers")
+set(CPACK_PACKAGE_CHECKSUM
+    "SHA256"
+    CACHE STRING "List of algorithms used to compute package checksums")
+set(CPACK_RESOURCE_FILE_LICENSE
+    "${PROJECT_SOURCE_DIR}/LICENSE.txt"
+    CACHE FILEPATH "License shown in graphical installers")
+set(CPACK_RESOURCE_FILE_README
+    "${PROJECT_SOURCE_DIR}/packaging/DESCRIPTION.txt"
+    CACHE FILEPATH "")
+
+# Initialize CPack
+# ##############################################################################
+include(CPack)
+
+# API for use with targets
+# ##############################################################################
+
+# piz_add_to_installer - Add a JUCE plugin to the installer
+function(piz_add_to_installer target)
+  get_target_property(friendly_name "${target}" JUCE_PRODUCT_NAME)
+  get_target_property(formats "${target}" JUCE_FORMATS)
+
+  foreach(format IN LISTS formats)
+    install(
+      TARGETS "${target}_${format}" #
+      BUNDLE
+      LIBRARY DESTINATION "${format}" #
+              COMPONENT "${target}_${format}")
+
+    # The installer structure will contain top-level nodes for each plugin
+    # format and sub-nodes for the individual plugins under each format.
+    cpack_add_component_group(
+      "${format}"
+      DISPLAY_NAME "${format}"
+      EXPANDED BOLD_TITLE)
+    cpack_add_component(
+      "${target}_${format}"
+      DISPLAY_NAME "${friendly_name}"
+      GROUP "${format}")
+  endforeach()
+endfunction()
