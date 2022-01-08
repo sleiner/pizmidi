54f31bdf0dd8387deddee8d64efe84429acb9fa0
Simon Leiner <simon@leiner.me>
2022-01-08 23:29:32 +0100

    Delete unused header files
    Signed-off-by: Simon Leiner <simon@leiner.me>

--- pizjuce/midiChords/ChordsKeyboardComponent.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHORDSKEYBOARDCOMPONENT_H
-#define CHORDSKEYBOARDCOMPONENT_H
-
-#include "JuceHeader.h"
-#include "MidiChords.h"
-
-
-
-#endif
--- pizjuce/midiChords/TriggerKeySelectorKeyboard.h
+++
@@ -1,8 +0,0 @@
-#ifndef TriggerKeySelectorKeyboard_H
-#define TriggerKeySelectorKeyboard_H
-
-#include "JuceHeader.h"
-#include "MidiChords.h"
-
-
-#endif
